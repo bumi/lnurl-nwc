bca565e3ea38a2a58ed2b10b57af0e479e4858aa
Michael Bumann <hello@michaelbumann.com>
2025-05-26 01:13:52 +0200

    Use deno k/v to store nwc connection
    this creates short LNURLs

--- src/main.ts
+++ src/main.ts
@@ -3,54 +3,60 @@ import { cors } from "hono/cors";
 import { serveStatic } from "hono/deno";
 import { secureHeaders } from "hono/secure-headers";
 import { nwc } from "npm:@getalby/sdk";
+import { bech32 } from "npm:bech32";
 import { LOG_LEVEL, logger, loggerMiddleware } from "./logger.ts";
+
+interface Lnurl {
+  name: string;
+  connectionSecret: string;
+  commentAllowed?: boolean;
+  payerDataAllowed?: boolean;
+  minSendable?: number;
+  maxSendable?: number;
+  successMessage?: string;
+}
 
 export const PORT = parseInt(Deno.env.get("PORT") || "8080");
 
+const kv = await Deno.openKv();
 const hono = new Hono();
+
+function encodeLnurl(url: string): string {
+  const words = bech32.toWords(new TextEncoder().encode(url));
+  return bech32.encode("lnurl", words, 2000);
+}
 
 hono.use(loggerMiddleware());
 hono.use(secureHeaders());
 hono.use(cors());
 
-function getLnurlMetadata(name: string): string {
+function getLnurlMetadata(lnurl: Lnurl): string {
   return JSON.stringify([
-    ["text/identifier", `${name}`],
-    ["text/plain", `Sats for ${name}`],
+    ["text/identifier", `${lnurl.name || "unknown"}`],
+    ["text/plain", `Sats for ${lnurl.name || "unknown"}`],
   ]);
 }
 
-function base64UrlEncode(str: string) {
-  const base64 = btoa(str);
-  return base64
-    .replace(/\+/g, "-")
-    .replace(/\//g, "_")
-    .replace(/=+$/, "");
-}
-
-function base64UrlDecode(str: string) {
-  let base64 = str
-    .replace(/-/g, "+")
-    .replace(/_/g, "/");
-  // Pad with '=' characters to make the length a multiple of 4
-  while (base64.length % 4) {
-    base64 += "=";
-  }
-  return atob(base64);
-}
-
-hono.get("/lnurlp/:name/:nwc", (c) => {
+hono.get("/lnurlp/:uuid", async (c) => {
   const url = new URL(c.req.url);
-  const encodedConnectionString = c.req.param("nwc");
-  const name = c.req.param("name");
-  return c.json({
+  const uuid = c.req.param("uuid");
+
+  const lnurlEntry = await kv.get<Lnurl>(["lnurls", uuid]);
+  const lnurl = lnurlEntry.value;
+  if (!lnurl) {
+    return c.json({ status: "ERROR", reason: "not found" });
+  }
+
+  const res = {
     tag: "payRequest",
-    commentAllowed: 255,
-    callback: `${url.origin}/callback/${name}/${encodedConnectionString}`,
-    minSendable: 1000,
-    maxSendable: 10000000000,
-    metadata: getLnurlMetadata(name),
-    payerData: {
+    commentAllowed: (lnurl.commentAllowed ? 255 : false),
+    callback: `${url.origin}/callback/${uuid}`,
+    minSendable: (lnurl.minSendable || 1) * 1000,
+    maxSendable: (lnurl.maxSendable || 1000000) * 1000,
+    metadata: getLnurlMetadata(lnurl),
+  };
+  if (lnurl.payerDataAllowed) {
+    res.payerData = {
       name: {
         mandatory: false,
       },
@@ -60,36 +66,40 @@ hono.get("/lnurlp/:name/:nwc", (c) => {
       pubkey: {
         mandatory: false,
       },
-    },
-  });
+    }
+  }
+
+  return c.json(res);
 });
 
-hono.get("/callback/:name/:nwc", async (c) => {
+hono.get("/callback/:uuid", async (c) => {
   try {
     const url = new URL(c.req.url);
-    const encodedConnectionString = c.req.param("nwc");
-    const name = c.req.param("name");
+    const uuid = c.req.param("uuid");
 
-    const amount = c.req.query("amount");
+    const lnurlEntry = await kv.get<Lnurl>(["lnurls", uuid]);
+    const lnurl = lnurlEntry.value;
+    if (!lnurl) {
+      throw new Error("not found");
+    }
+
+    const amount = +c.req.query("amount");
     const comment = c.req.query("comment") || "";
     const payerData = c.req.query("payerdata")
       ? JSON.parse(c.req.query("payerdata") || "")
       : null;
 
-    logger.debug("LNURLp callback", { name, amount, comment });
-
     if (!amount) {
       throw new Error("No amount provided");
     }
-
-    let connectionSecret = base64UrlDecode(encodedConnectionString);
-
-    if (!connectionSecret.startsWith("nostr+walletconnect://")) {
-      connectionSecret = `nostr+walletconnect://${connectionSecret}`;
+    if (lnurl.maxSendable && amount > lnurl.maxSendable) {
+      throw new Error("Amount too high");
     }
-    if (!connectionSecret.match(/relay=/)) {
-      connectionSecret = `${connectionSecret}&relay=wss://relay.getalby.com/v1`
+    if (lnurl.minSendable && amount < lnurl.minSendable) {
+      throw new Error("Amount too low");
     }
+
+    let connectionSecret = lnurl.connectionSecret;
     const nwcClient = new nwc.NWCClient({
       nostrWalletConnectUrl: connectionSecret,
     });
@@ -103,24 +113,38 @@ hono.get("/callback/:name/:nwc", async (c) => {
       },
     });
 
-    return c.json({
+    const res = {
       verify:
-        `${url.origin}/verify/${name}/${encodedConnectionString}/${transaction.payment_hash}`,
+        `${url.origin}/verify/${uuid}/${transaction.payment_hash}`,
       routes: [],
       pr: transaction.invoice,
-    });
+    }
+    if (lnurl.successMessage) {
+      res.successAction = {
+        "tag": "message",
+        "message": lnurl.successMessage
+      }
+    };
+
+    return c.json(res)
   } catch (error) {
     return c.json({ status: "ERROR", reason: "" + error });
   }
 });
 
-hono.get("/verify/:name/:nwc/:payment_hash", async (c) => {
+hono.get("/verify/:uuid/:payment_hash", async (c) => {
   try {
-    const encodedConnectionString = c.req.param("nwc");
+    const uuid = c.req.param("uuid");
+
+    const lnurlEntry = await kv.get<Lnurl>(["lnurls", uuid]);
+    const lnurl = lnurlEntry.value;
+    if (!lnurl) {
+      return c.json({ status: "ERROR", reason: "not found" });
+    }
+
     const paymentHash = c.req.param("payment_hash");
 
-    const connectionSecret = base64UrlDecode(encodedConnectionString);
-
+    let connectionSecret = lnurl.connectionSecret;
     const nwcClient = new nwc.NWCClient({
       nostrWalletConnectUrl: connectionSecret,
     });
@@ -141,6 +165,45 @@ hono.get("/verify/:name/:nwc/:payment_hash", async (c) => {
   }
 });
 
+hono.post("/create", async (c) => {
+  const url = new URL(c.req.url);
+
+  const json = await c.req.json();
+
+  const uuid = crypto.randomUUID();
+
+  const lnurl = {
+    name: json.name,
+    connectionSecret: json.connectionSecret,
+    minSendable: json.minSendable,
+    maxSendable: json.maxSendable,
+    commentAllowed: json.commentAllowed,
+    payerDataAllowed: json.payerDataAllowed
+  };
+
+  try {
+    const nwcClient = new nwc.NWCClient({
+      nostrWalletConnectUrl: lnurl.connectionSecret,
+    });
+    const info = await nwcClient.getInfo();
+    if (!info.methods || info.methods.includes("pay_invoice") || info.methods.includes("pay_keysend")) {
+      throw new Error("Use a readonly connection");
+    }
+  } catch (error) {
+    return c.json({ error: "review your connection secret and make sure it's a readonly connection" });
+  }
+
+  await kv.set(["lnurls", uuid], lnurl);
+
+  const endpoint = `${url.origin}/lnurlp/${uuid}`;
+  const endpointEncoded = encodeLnurl(endpoint);
+  return c.json({
+    uuid,
+    url: endpoint,
+    lnurl: endpointEncoded
+  });
+});
+
 hono.get("/ping", (c) => {
   return c.body("pong", 200);
 });
@@ -149,7 +212,7 @@ hono.get("/robots.txt", (c) => {
   return c.body("User-agent: *\nDisallow: /", 200);
 });
 
-hono.use("/", serveStatic({ path: "./index.html" }));
+hono.use("/", serveStatic({ path: "./lnurl.html" }));
 
 hono.use("/favicon.ico", serveStatic({ path: "./favicon.ico" }));
 
